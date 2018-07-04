3892f0177e29df83f29efc1050f2de1cdf80ffe9
shawuha <shulykalexandra@gmail.com>
2018-07-04 11:08:04 +0300

    commit

---
+++ DZ00/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b,res;
+           // int res;
+            char znack;          
+            Console.WriteLine("Введите чиcло");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите действие");
+            znack = Convert.ToChar(Console.ReadLine());
+            if(znack == '+')
+                {res = a + b;
+                Console.WriteLine("Сумма:"+res);
+            }
+                else if(znack == '-')
+                {res = a - b;
+                Console.WriteLine("Разность:"+res);
+            }
+                else if(znack == '*')
+                {res = a * b;
+                Console.WriteLine("Произведение:"+res);
+            }
+                else if(znack == '/')
+                {res = a / b;
+                Console.WriteLine("Деление:"+res);
+            }
+
+    }
+  }
+}
