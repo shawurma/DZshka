c9e75c646f8b5b2e6b8c5c56fcf0a7f515052608
shawuha <shulykalexandra@gmail.com>
2018-07-05 18:17:09 +0300

    comt

---
+++ DZ01/Factorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int n = 5;
+           int factorial = 1; 
+           for (int i = 2; i <= n; i++) 
+           { factorial = factorial * i; }
+            Console.WriteLine("Полученый факториал из"  + n +" = " + factorial);
+    }
+}
+}
